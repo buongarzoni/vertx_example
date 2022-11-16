61da61f17e10cb7f648f38168664ffab9b3fad1d
buongarzoni <christian.buongarzoni@gmail.com>
2022-11-16 01:58:14 -0300

    feat(app): add router for /status path

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,9 +1,14 @@
 import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
 
 fun main() {
     val vertx = Vertx.vertx()
-    vertx.createHttpServer().requestHandler{
-        it.response().end("OK :D")
-    }.listen(8081)
+    val router = Router.router(vertx)
+
+    router.get("/status").handler {
+        it.response().setStatusCode(200).end("OK :D")
+    }
+    vertx.createHttpServer().requestHandler(router).listen(8081)
+
     println("open http://localhost:8081")
 }
