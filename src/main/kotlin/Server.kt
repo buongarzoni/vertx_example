e62ee56dc0938428802c6c6a37c0e14b4d9270d2
buongarzoni <christian.buongarzoni@gmail.com>
2022-11-16 02:02:34 -0300

    feat(app): add json response

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,14 +1,20 @@
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
 
 fun main() {
     val vertx = Vertx.vertx()
     val router = Router.router(vertx)
 
     router.get("/status").handler {
-        it.response().setStatusCode(200).end("OK :D")
+        it.okStatusResponse()
     }
     vertx.createHttpServer().requestHandler(router).listen(8081)
 
     println("open http://localhost:8081")
 }
+
+private fun RoutingContext.okStatusResponse() = response().setStatusCode(200).end(okStatus())
+private fun okStatus() = json { obj("status" to "OK ^-^") }.toString()
