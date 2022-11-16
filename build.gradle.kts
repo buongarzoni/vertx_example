b0ab68699409247b3efc8bbdaa8dd812e3e857ca
buongarzoni <christian.buongarzoni@gmail.com>
2022-11-16 02:54:10 -0300

    feat(app): add dependencies for test requests

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,9 @@ dependencies {
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("io.vertx:vertx-web-client:$vertxVersion")
 }
 
 tasks.test {
